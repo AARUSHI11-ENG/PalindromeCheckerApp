7f2b4b33711b5c650b717d6a7de42732219eda70
AARUSHI11-ENG <aarushijhawar75@gmail.com>
2026-02-12 16:06:24 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+
+/*
+Use Case 1 : Palindrome Checker App
+Author : Aarushi Jhawar
+Date : 12/02/26
+Reg no : RA2411026010258
+ */
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
