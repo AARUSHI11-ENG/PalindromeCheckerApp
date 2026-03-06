67fd0f0a1fe81ef151109921e6c90406c4992315
AARUSHI11-ENG <aarushijhawar75@gmail.com>
2026-03-06 14:35:30 +0530

    UC10: Case-Insensitive & Space-Ignored Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,21 @@
 
 /*
-Use Case 9 :"Recursive Palindrome Checker"
+Use Case 10 :"Case-Insensitive & Space-Ignored Palindrome"
 Author : Aarushi Jhawar
 Date : 05/03/26
 Reg no : RA2411026010258
- if (start >= end) {
-            return true;
-        }
+ int start = 0;
+            int end = word.length() - 1;
+            boolean isPalindrome = true;
 
-        if (word.charAt(start) != word.charAt(end)) {
-            return false;
-        }
-
-        return isPalindrome(word, start + 1, end - 1);
-    }
+            while (start < end) {
+                if (word.charAt(start) != word.charAt(end)) {
+                    isPalindrome = false;
+                    break;
+                }
+                start++;
+                end--;
+            }
  */
 
 import java.util.Queue;
@@ -23,31 +25,30 @@ import java.util.Deque;
 import java.util.ArrayDeque;
 
 public class PalindromeCheckerApp {
+        public static void main(String[] args) {
 
-    static boolean isPalindrome(String word, int start, int end) {
+            String input = "A man a plan a canal panama";
 
-        if (start >= end) {
-            return true;
+            String word = input.replaceAll("\\s+", "").toLowerCase();
+
+            int start = 0;
+            int end = word.length() - 1;
+            boolean isPalindrome = true;
+
+            while (start < end) {
+                if (word.charAt(start) != word.charAt(end)) {
+                    isPalindrome = false;
+                    break;
+                }
+                start++;
+                end--;
+            }
+            System.out.println("Input: "+ word);
+
+            if (isPalindrome) {
+                System.out.println("Result: The given string is a Palindrome.");
+            } else {
+                System.out.println("Result: The given string is NOT a Palindrome.");
+            }
         }
-
-        if (word.charAt(start) != word.charAt(end)) {
-            return false;
-        }
-
-        return isPalindrome(word, start + 1, end - 1);
     }
-
-    public static void main(String[] args) {
-
-        String word = "madam";
-
-        boolean result = isPalindrome(word, 0, word.length() - 1);
-        System.out.println("Input :"+word);
-
-        if (result) {
-            System.out.println("Result: The given string is a Palindrome.");
-        } else {
-            System.out.println("Result: The given string is NOT a Palindrome.");
-        }
-    }
-}
